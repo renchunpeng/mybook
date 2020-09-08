3c4523ffb34d854d3a624588ae9b5b96d81eaf8d
renchunpeng <1258590898@qq.com>
2020-09-08 14:15:50 +0800

    修改测试包

--- src/test/java/com/rencp/mybook/MybookApplicationTests.java
+++ src/test/java/com/rencp/mybook/MybookApplicationTests.java
@@ -5,7 +5,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import tk.mybatis.spring.annotation.MapperScan;
--- src/test/java/com/rencp/mybook/RcpTest.java
+++ src/test/java/com/rencp/mybook/RcpTest.java
@@ -6,7 +6,7 @@ import com.rencp.mybook.pojo.Book;
 import com.rencp.mybook.pojo.Chapter;
 import com.rencp.mybook.service.ThirdPartyService;
 import com.rencp.mybook.service.UtilService;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
